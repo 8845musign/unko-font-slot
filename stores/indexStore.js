c3cfcc137ff46229af496b69a4a5422b6c4188ee
8845musign <8845musign@gmail.com>
2017-06-17 16:24:00 +0900

    Add dev tool setting

--- stores/indexStore.js
+++ stores/indexStore.js
@@ -1,7 +1,14 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, compose } from 'redux'
 
 import gameReducer from '../modules/game'
 import memberReducer from '../modules/member'
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+
+const enhancer = composeEnhancers()
 
 const rootReducer = combineReducers({
   game: gameReducer,
@@ -9,7 +16,7 @@ const rootReducer = combineReducers({
 })
 
 const indexStore = () => {
-  return createStore(rootReducer)
+  return createStore(rootReducer, {}, enhancer)
 }
 
 export default indexStore
