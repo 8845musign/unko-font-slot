14ca55b76bd9f03b37543f9172b4400aca699638
8845musign <8845musign@gmail.com>
2017-07-02 15:40:44 +0900

    Split inital state file.

--- stores/indexStore.js
+++ stores/indexStore.js
@@ -13,6 +13,8 @@ import slotReducer, { middlewares as slotMiddlewares } from '../modules/slot'
 import confettiReducer, { middlewares as confettiMiddlewares } from '../modules/confetti'
 import resultReducer from '../modules/result'
 
+import initalState from './initialState'
+
 const composeEnhancers =
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -34,28 +36,7 @@ const rootReducer = combineReducers({
 })
 
 const indexStore = () => {
-  return createStore(rootReducer, {
-    game: {
-      selected: []
-    },
-    members: {
-      byId: {
-        1: {
-          'name': 'Dummy1',
-          'image': '/static/images/photos/dummy-1.jpg'
-        },
-        2: {
-          'name': 'Dummy2',
-          'image': '/static/images/photos/dummy-2.jpg'
-        },
-        3: {
-          'name': 'Dummy3',
-          'image': '/static/images/photos/dummy-3.jpg'
-        }
-      },
-      allIds: [1, 2, 3]
-    }
-  }, enhancer)
+  return createStore(rootReducer, initalState, enhancer)
 }
 
 export default indexStore
